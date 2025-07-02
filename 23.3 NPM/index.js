562f81aaf66d7777f3639f4325ebfc00516a55e6
amusarri <186473330+amusarri@users.noreply.github.com>
2025-07-02 10:17:11 -0400

    Completed 23.3

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from 'sillyname';
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+
+import superheroes, { randomSuperhero } from 'superheroes';
+var superheroName = randomSuperhero();
+
+console.log(`It's ${superheroName}!`);
