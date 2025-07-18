be787dacb97811a37b9b0391a28ec0774eadca14
amusarri <186473330+amusarri@users.noreply.github.com>
2025-07-18 19:50:53 -0400

    Completed functionality

--- 29. Capstone 4/index.js
+++ 29. Capstone 4/index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -13,9 +14,8 @@ app.get("/", (req, res) => {
 
   app.post("/wallpaper", async (req, res) => {
     try {
-      const result = await axios.get("https://picsum.photos/seed/picsum/");
-      console.log(result.data)
-      res.render("index.ejs");
+      const result = await axios.get("https://api.adviceslip.com/advice");
+      res.render("index.ejs", {data: result.data.slip.advice});
     } catch (error) {
       res.render("index.ejs");
       console.log(error.response.data)
